a155b27c908523c5c222fd3f3c21fd3437f8cb9c
Ribonney <57567251+Ribonney@users.noreply.github.com>
2020-12-01 11:40:26 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-PROXY_URL = 'https://54.37.131.45:3128'
-REQUEST_KWARGS = {'proxy_url': 'https://54.37.131.45:3128'}
 TOKEN = "1470628993:AAHOjzwU5D0vAzEMomYbCbHVr8LjjyfoseY"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
